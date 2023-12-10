2519aa09e695d4016fb2272bf40d62b89bb6184c
JohannaB95 <johanna_b_g@hotmail.com>
2023-12-10 13:58:08 -0500

    Documentación del código

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
+//Se carga la libreria colors para permitir el uso de estilos de colores en la terminal
 require('colors');
 
+//Se carga el código que contiene el archivo oper_math.js en la carpeta modules.
 const math = require('./modules/oper_math.js');
 
+//Se declara una variabale llamada resultado1 que almacenara el número aleatorio obtenido en la función numeroAleatorio1()
 let resultado1 = numeroAleatorio1();
 
+//Se declara una función llamada numeroAleatorio1 sin parámetros
 function numeroAleatorio1() {
+  /*Se retornara un número aleatorio que se encuentre entre 1 y 9.
+  Se utiliza Math.floor para redondear un número hacia el entero anterior.
+  Se utiliza Math.random para obtener un número decimal aleatorio en el rango [0, 9]*/
   return Math.floor(Math.random()*10);
 }
 
+//Se declara una variabale llamada resultado1 que almacenara el número aleatorio obtenido en la función numeroAleatorio2()
 let resultado2 = numeroAleatorio2();
 
+//Se declara una función llamada numeroAleatorio2 sin parámetros
 function numeroAleatorio2() {
+    /*Se retornara un número aleatorio que se encuentre entre 1 y 9.
+    Se utiliza Math.floor para redondear un número hacia el entero anterior.
+    Se utiliza Math.random para obtener un número decimal aleatorio en el rango [0, 9]*/
     return (Math.floor(Math.random()*10));
 }
 
+//Se limpia la consola
 console.clear( );
 
+//Se imprime en la consola el contenido del objeto math
 console.log(math);
 
-const main = async( ) => {
+//Se declara una función flecha main asincrónica 
+const main = async() => {
 
+   //Se imprime en la consola un recuadro con un título
     console.log('****************************************'.blue);
     console.log('*       '.blue,'Operaciones Matemáticas'.bgGreen, '      *'.blue);
     console.log('****************************************\n'.blue);
     
+     /*Se imprime en la consola un recuadro con un subtítulo, los nombres de las operaciones que se realizarán,
+     los números aleatorios con los que se realizarán las operaciones y los resultados de cada una de las 
+     operaciones mátematicas; se da interfaz y se aplican colores*/
     console.log('****************************************'.grey);
     console.log('*                                      *'.grey)
     console.log('*      '.grey, 'Números generados'.bgBlue, resultado1 + " " + 'y'.grey, resultado2 + '        *'.grey);
@@ -39,4 +58,5 @@ const main = async( ) => {
     console.log('****************************************'.grey);
 }
 
+//Se llama a la función main
 main();
--- modules/oper_math.js
+++ modules/oper_math.js
@@ -1,29 +1,45 @@
-const Math = {}; 
+//Se declara un objeto vacío llamado Math. Este objeto se utiliza para agrupar funciones relacionadas con operaciones matemáticas.
+const Math = {};
 
+//Se declara una función llamada suma que tiene dos parámetros resultado1 y resultado2
 function suma(resultado1,resultado2) {
+    //Se retorna el resultado de la suma entre resultado1 y resultado2
     return resultado1 + resultado2;
 }
 
+//Se declara una función llamada resta que tiene dos parámetros resultado1 y resultado2
 function resta(resultado1,resultado2) {
+    //Se retorna el resultado de la resta entre resultado1 y resultado2
     return resultado1 - resultado2;
 }
 
+//Se declara una función llamada multiplicacion que tiene dos parámetros resultado1 y resultado2
 function multiplicacion(resultado1,resultado2) {
+    //Se retorna el resultado de la multiplicación entre resultado1 y resultado2
     return resultado1 * resultado2;
 }
 
+//Se declara una función llamada división que tiene dos parámetros resultado1 y resultado2
 function division(resultado1,resultado2) {
+    //Se utiliza if para verificar si resultado2 es igual a 0 y si resultado1 es igual a 0
     if (resultado2 == 0 && resultado1 == 0) {
+        //Si la condición se cumple, se retornara un mensaje de no aplica
         return "N/A"
+    //Se evalua si resultado2 es igual a cero
     } else if (resultado2 == 0) { 
+        //Si la condición se cumple, se retornara un mensaje de no aplica
         return "N/A"
     }
+        //Se retorna el resultado de la división entre resultado1 y resultado2
         return resultado1 / resultado2;
 }
 
+/*Se asignan las funciones suma, resta, multiplicacion, y division como propiedades del objeto Math. Esto crea 
+métodos asociados con el objeto Math.*/
 Math.suma = suma;
 Math.resta = resta;
 Math.multiplicacion = multiplicacion;
 Math.division = division;
 
-module.exports = Math
+//Se exporta el objeto Math para que pueda ser utilizado en otros archivos. Esto se logra usando module.exports.
+module.exports = Math;
